d312ef5bb5eea20ab17449dae0656151a29131cd
Artin Boghosian <aboghosian@tweetmyjobs.com>
2014-02-25 14:01:47 -0800

    use memory adapter for ActiveMapper::Relation spec

--- spec/active_mapper/relation_spec.rb
+++ spec/active_mapper/relation_spec.rb
@@ -1,34 +1,42 @@
 require 'spec_helper'
 
 describe ActiveMapper::Relation do
-  let(:adapter) { double('Adapter') }
+  let(:adapter) { ActiveMapper::Adapter::Memory.new }
   let(:user) { User.new(name: 'user', age: 28) }
-  let(:query) { proc { |user| user.age > 18 } }
-  let(:relation) { ActiveMapper::Relation.new(User, adapter, &query) }
+  let(:other_user) { User.new(name: 'other', age: 35) }
 
-  before { adapter.stub(unserialize: user) }
+  before do
+    user.id  = adapter.insert(User, user)
+    other_user.id = adapter.insert(User, other_user)
+  end
+
+  def create_relation(&block)
+    ActiveMapper::Relation.new(User, adapter, &block)
+  end
 
   describe '#any?' do
     it 'is true when there are matching objects' do
-      expect(adapter).to receive(:count).with(User).and_return(1)
+      relation = create_relation { |user| user.age == 28 }
       expect(relation.any?).to be_true
     end
 
     it 'is false when there are no matching objects' do
-      expect(adapter).to receive(:count).with(User).and_return(0)
+      relation = create_relation { |user| user.age == 18 }
       expect(relation.any?).to be_false
     end
   end
 
   describe '#none?' do
     it 'is true when there are no matching objects' do
-      expect(adapter).to receive(:count).with(User).and_return(0)
+      relation = create_relation { |user| user.age < 18 }
+
       expect(relation.none?).to be_true
       expect(relation.empty?).to be_true
     end
 
     it 'is false when there are matching objects' do
-      expect(adapter).to receive(:count).with(User).and_return(1)
+      relation = create_relation { |user| user.age == 28 }
+
       expect(relation.none?).to be_false
       expect(relation.empty?).to be_false
     end
@@ -36,171 +44,129 @@ describe ActiveMapper::Relation do
 
   describe '#one?' do
     it 'is true when there is one matching object' do
-      expect(adapter).to receive(:count).with(User).and_return(1)
+      relation = create_relation { |user| user.age == 28 }
+
       expect(relation.one?).to be_true
     end
 
     it 'is false when there are no matching objects' do
-      expect(adapter).to receive(:count).with(User).and_return(0)
+      relation = create_relation { |user| user.age < 18 }
+
       expect(relation.one?).to be_false
     end
 
     it 'is false when there are is more than one matching object' do
-      expect(adapter).to receive(:count).with(User).and_return(2)
+      relation = create_relation { |user| user.age > 18 }
+
       expect(relation.one?).to be_false
     end
   end
 
   describe '#count' do
     it 'counts the number of matching objects' do
-      expect(adapter).to receive(:count).with(User) do |&block|
-        expect(block).to eq(query)
-      end.and_return(10)
+      relation = create_relation { |user| user.age == 28 }
 
-      expect(relation.count).to eq(10)
-      expect(relation.length).to eq(10)
-      expect(relation.size).to eq(10)
+      expect(relation.count).to eq(1)
+      expect(relation.length).to eq(1)
+      expect(relation.size).to eq(1)
     end
   end
 
   describe '#min' do
     it 'calculates the minimum value' do
-      expect(adapter).to receive(:minimum).with(User, :age) do |&block|
-        expect(block).to eq(query)
-      end.and_return(28)
-
-      expect(relation.min(:age)).to eql(28)
+      expect(create_relation.min(:age)).to eql(28)
     end
   end
 
   describe '#max' do
     it 'calculates the maximum value' do
-      expect(adapter).to receive(:maximum).with(User, :age) do |&block|
-        expect(block).to eq(query)
-      end.and_return(35)
-
-      expect(relation.max(:age)).to eq(35)
+      expect(create_relation.max(:age)).to eq(35)
     end
   end
 
   describe '#minmax' do
     it 'calculates the minimum and maximum values' do
-      expect(adapter).to receive(:minimum).with(User, :age) do |&block|
-        expect(block).to eq(query)
-      end.and_return(28)
-
-      expect(adapter).to receive(:maximum).with(User, :age) do |&block|
-        expect(block).to eq(query)
-      end.and_return(35)
-
-      expect(relation.minmax(:age)).to eq([28, 35])
+      expect(create_relation.minmax(:age)).to eq([28, 35])
     end
   end
 
   describe '#avg' do
     it 'calculates the average value' do
-      expect(adapter).to receive(:average).with(User, :age) do |&block|
-        expect(block).to eq(query)
-      end.and_return(31.5)
-
-      expect(relation.avg(:age)).to eq(31.5)
+      expect(create_relation.avg(:age)).to eq(31.5)
     end
   end
 
   describe '#sum' do
     it 'calculates the total value' do
-      expect(adapter).to receive(:sum).with(User, :age) do |&block|
-        expect(block).to eq(query)
-      end.and_return(63)
-
-      expect(relation.sum(:age)).to eq(63)
+      expect(create_relation.sum(:age)).to eq(63)
     end
   end
 
   describe '#drop' do
     it 'offsets by the number of objects' do
-      expect(adapter).to receive(:where).with(User, hash_including(offset: 10)).and_return([user])
-
-      relation.drop(10).to_a
+      expect(create_relation.drop(1).to_a).to eq([other_user])
     end
   end
 
   describe '#first' do
     it 'returns the first matching object' do
-      expect(adapter).to receive(:where).with(User, hash_including(offset: 0, limit: 1)).and_return([user])
-      expect(relation.first).to eq(user)
+      expect(create_relation.first).to eq(user)
     end
 
     it 'returns the first number of matching objects' do
-      expect(adapter).to receive(:where).with(User, hash_including(offset: 0, limit: 3)).and_return([user])
-
-      relation.first(3)
+      expect(create_relation.first(2)).to eq([user, other_user])
     end
   end
 
   describe '#last' do
     it 'returns the last matching object' do
-      expect(adapter).to receive(:where).with(User, hash_including(offset: 0, limit: 1, order: [:id, :desc])).and_return([user])
-      expect(relation.last).to eq(user)
+      expect(create_relation.last).to eq(other_user)
     end
 
     it 'returns the last number of matching objects' do
-      expect(adapter).to receive(:where).with(User, hash_including(offset: 0, limit: 3, order: [:id, :desc])).and_return([user])
-
-      relation.last(3)
+      expect(create_relation.last(2)).to eq([other_user, user])
     end
   end
 
   describe '#take' do
     it 'limits the number of records returned' do
-      expect(adapter).to receive(:where).with(User, hash_including(limit: 10)).and_return([user])
-
-      relation.take(10).to_a
+      expect(create_relation.take(1).to_a).to eq([user])
     end
   end
 
   describe '#to_a' do
     it 'finds all matching objects' do
-      expect(adapter).to receive(:where).with(User, an_instance_of(Hash)) do |&block|
-        expect(block).to eq(query)
-      end.and_return([user])
-
-      expect(relation.to_a).to eq([user])
+      expect(create_relation { |user| user.age == 28 }.to_a).to eq([user])
     end
   end
 
   describe '#map' do
     it 'maps the objects' do
-      expect(adapter).to receive(:where).with(User, an_instance_of(Hash)).and_return([user])
-      expect(relation.map(&:name)).to eq(['user'])
+      expect(create_relation.map(&:name)).to eq(['user', 'other'])
     end
   end
 
   describe '#each' do
     it 'iterates through the objects' do
-      expect(adapter).to receive(:where).and_return([user])
-      
-      relation.each { |u| expect(u).to eq(user) } 
+      create_relation { |user| user.age == 18 }.each { |u| expect(u).to eq(user) }
     end
   end
 
   describe '#sort' do
     it 'sets the attribute to order by' do
-      expect(adapter).to receive(:where).with(User, hash_including(order: [:name, :asc])) do |&block|
-        expect(block).to eq(query)
-      end.and_return([user])
+      relation = create_relation.sort(:name).to_a
 
-      relation.sort(:name).to_a
+      expect(relation.first).to eq(other_user)
+      expect(relation.last).to eq(user)
     end
   end
 
   describe '#reverse' do
     it 'sets the opposite direction to order by' do
-      expect(adapter).to receive(:where).with(User, hash_including(order: [:name, :desc])) do |&block|
-        expect(block).to eq(query)
-      end.and_return([user])
+      relation = create_relation.sort(:age).reverse.to_a
 
-      relation.sort_by(:name).reverse.to_a
+      expect(relation.first).to eq(other_user)
+      expect(relation.last).to eq(user)
     end
   end
 end
