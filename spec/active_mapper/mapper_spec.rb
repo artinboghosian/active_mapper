b4623386c2c1520d0958fbfa9398319a5a26497c
Artin Boghosian <aboghosian@tweetmyjobs.com>
2014-02-25 13:46:35 -0800

    use memory adapter instead of mock adapter for ActiveMapper::Mapper test

--- spec/active_mapper/mapper_spec.rb
+++ spec/active_mapper/mapper_spec.rb
@@ -1,199 +1,193 @@
 require 'spec_helper'
 describe ActiveMapper::Mapper do
-  let(:adapter) { double('Adapter') }
+  #let(:adapter) { double('Adapter') }
+  let(:adapter) { ActiveMapper::Adapter::Memory.new }
   let(:mapper) { ActiveMapper::Mapper.new(User, adapter) }
-  let(:user) { User.new }
+  let(:user) { User.new(name: 'user', age: 28) }
+  let(:other_user) { User.new(name: 'other', age: 35) }
 
-  before { adapter.stub(unserialize: user, where: [user]) }
+  before do
+    mapper.save(user)
+    mapper.save(other_user)
+  end
 
   describe '#all?' do
-    pending 'how to test'
+    it 'is true when all objects match' do
+      expect(mapper.all? { |user| user.age > 18 }).to be_true
+    end
+
+    it 'is false when all objects do not match' do
+      expect(mapper.all? { |user| user.age > 28 }).to be_false
+    end
   end
 
   describe '#any?' do
     it 'is true when any objects match' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(1)
-
-      expect(mapper.any?).to be_true
+      expect(mapper.any? { |user| user.age == 28 }).to be_true
     end
 
     it 'is false when no objects match' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(0)
-
-      expect(mapper.any?).to be_false
+      expect(mapper.any? { |user| user.age > 35 }).to be_false
     end
   end
 
   describe '#none?' do
     it 'is true when no objects match' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(0)
-
-      expect(mapper.none?).to be_true
+      expect(mapper.none? { |user| user.age == 18 }).to be_true
     end
 
     it 'is false when any objects match' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(1)
-
-      expect(mapper.none?).to be_false
+      expect(mapper.none? { |user| user.age == 28 }).to be_false
     end
   end
 
   describe '#one?' do
     it 'is true when one object matches' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(1)
-
-      expect(mapper.one?).to be_true
+      expect(mapper.one? { |user| user.age == 28 }).to be_true
     end
 
     it 'is false when no objects match' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(0)
-
-      expect(mapper.one?).to be_false
+      expect(mapper.one? { |user| user.age > 35 }).to be_false
     end
 
     it 'is false when more than one object matches' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(2)
-
-      expect(mapper.one?).to be_false
+      expect(mapper.one? { |user| user.age > 18 }).to be_false
     end
   end
 
   describe '#count' do
-    it 'counts the matching objects' do
-      ActiveMapper::Relation.any_instance.should_receive(:count).and_return(10)
+    it 'counts the all objects' do
+      expect(mapper.count).to eq(2)
+    end
 
-      expect(mapper.count).to eq(10)
+    it 'counts matching objects' do
+      expect(mapper.count { |user| user.age < 35 }).to eq(1)
     end
   end
 
   describe '#min' do
     it 'finds the minimum value' do
-      ActiveMapper::Relation.any_instance.should_receive(:min).and_return(28)
-
       expect(mapper.min(:age)).to eq(28)
     end
   end
 
   describe '#max' do
     it 'finds the maximum value' do
-      ActiveMapper::Relation.any_instance.should_receive(:max).and_return(35)
-
       expect(mapper.max(:age)).to eq(35)  
     end
   end
 
   describe '#minmax' do
     it 'finds the minium and maximum values' do
-      ActiveMapper::Relation.any_instance.should_receive(:min).and_return(28)
-      ActiveMapper::Relation.any_instance.should_receive(:max).and_return(35)
-
       expect(mapper.minmax(:age)).to eq([28, 35])
     end
   end
 
   describe '#average' do
     it 'finds the average value' do
-      ActiveMapper::Relation.any_instance.should_receive(:avg).and_return(31.5)
-
       expect(mapper.avg(:age)).to eq(31.5)
     end
   end
 
   describe '#sum' do
     it 'finds the total value' do
-      ActiveMapper::Relation.any_instance.should_receive(:sum).and_return(63)
-
       expect(mapper.sum(:age)).to eq(63)
     end
   end
 
   describe '#find' do
     it 'finds the object with the matching id' do
-      ActiveMapper::Relation.any_instance.should_receive(:to_a).and_return([user])
-
-      expect(mapper.find(1)).to eq(user)
+      expect(mapper.find(user.id)).to eq(user)
     end
 
     it 'finds the first matching object' do
-      ActiveMapper::Relation.any_instance.should_receive(:to_a).and_return([user])
-
       expect(mapper.find { |user| user.age > 21 }).to eq(user)
     end
   end
 
   describe '#find_all' do
-    it 'finds all matching objects' do
-      ActiveMapper::Relation.any_instance.should_receive(:to_a).and_return([user])
+    it 'finds all objects' do
+      expect(mapper.find_all.to_a).to eq([user, other_user])
+    end
 
-      expect(mapper.find_all { |user| user.age > 18 }.to_a).to eq([user])
+    it 'finds all matching objects' do
+      expect(mapper.find_all { |user| user.age < 30 }.to_a).to eq([user])
     end
   end
 
   describe '#last' do
     it 'finds the last matching object' do
-      ActiveMapper::Relation.any_instance.should_receive(:to_a).and_return([user])
-
-      expect(mapper.last { |user| user.age > 18 }).to eq(user)
+      expect(mapper.last { |user| user.age > 18 }).to eq(other_user)
     end
   end
 
   describe '#select' do
-    pending 'how to test'
+    it 'finds all matching objects' do
+      expect(mapper.select { |user| user.age < 30 }.to_a).to eq([user])
+    end
   end
 
   describe '#reject' do
-    pending 'how to test'
+    it 'finds all non matching objects' do
+      expect(mapper.reject { |user| user.age == 28 }.to_a).to eq([other_user])
+    end
   end
 
   describe '#delete' do
     it 'deletes the object' do
-      expect(adapter).to receive(:delete).with(User, user)
       mapper.delete(user)
+
+      expect(mapper.find(user.id)).to be_nil
     end
   end
 
   describe '#delete_if' do
     it 'deletes all matching objects' do
-      expect(adapter).to receive(:delete_all).with(User) do |&block|
-        expect(block).to be_a(Proc)
-      end
+      mapper.delete_if { |user| user.age == 28 }
 
-      mapper.delete_if { |user| user.age > 18 }
+      expect(mapper.find(user.id)).to be_nil
+      expect(mapper.find(other_user.id)).to eq(other_user)
     end
   end
 
   describe '#clear' do
     it 'deletes all objects' do
-      expect(adapter).to receive(:delete_all).with(User) do |&block|
-        expect(block).to be_nil
-      end
-
       mapper.clear
+
+      expect(mapper.count).to eq(0)
     end
   end
 
   describe '#keep_if' do
-    pending 'how to test'
+    it 'deletes all non matching objects' do
+      mapper.keep_if { |user| user.age == 28 }
+
+      expect(mapper.find(user.id)).to eq(user)
+      expect(mapper.find(other_user.id)).to be_nil
+    end
   end
 
   describe '#save' do
+    before { mapper.clear }
+
     it 'does not save invalid objects' do
       user.stub(valid?: false)
       expect(adapter).to_not receive(:insert)
 
-      mapper.save(user)
+      expect(mapper.save(user)).to be_false
     end
 
     it 'creates unpersisted objects that are valid' do
       user.stub(valid?: true)
+      user.id = nil
 
       expect(adapter).to receive(:insert).with(User, user).and_return(1)
-      expect { mapper.save(user) }.to change(user, :id).to eq(1)
+      expect { mapper.save(user) }.to change(user, :id)
     end
 
     it 'updates persisted objects that are valid' do
       user.stub(valid?: true)
-      user.id = 1
+      mapper.save(user)
 
       expect(adapter).to receive(:update).with(User, user)
       expect { mapper.save(user) }.to_not change(user, :id)
