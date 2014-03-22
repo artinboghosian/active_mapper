823c69901b74ffc35bdf012a16ab52813154ca30
Artin Boghosian <aboghosian@tweetmyjobs.com>
2014-03-21 17:59:21 -0700

    remove activerecord adapter

--- lib/active_mapper/adapter.rb
+++ lib/active_mapper/adapter.rb
@@ -1,5 +1,4 @@
 require 'active_mapper/adapter/memory'
-require 'active_mapper/adapter/active_record'
 require 'active_mapper/adapter/arel'
 
 module ActiveMapper
--- spec/features/active_record_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe 'ActiveMapper with ActiveRecord adapter' do
-  setup_active_record('active_record_integration')
-
-  it_should_behave_like 'ActiveMapper Integration', ActiveMapper::Adapter::ActiveRecord.new
-end
